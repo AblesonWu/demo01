f36b29f63597a4718da6002504eee24ee2bf3214
Wuran <wuranlai1231@outlook.com>
2020-04-29 16:48:03 +0800

    Update the vignettes and added documentation for printer

--- R/printer.R
+++ R/printer.R
@@ -1,14 +1,16 @@
 #' The Printer Function
 #'
-#' @param r
-#' @param x
+#' @param r what you want in first column
+#' @param x what you want in second column
 #'
 #' @return
 #' @export
-#' @examples
 #'
+#' @importFrom tibble tibble
+#' @examples
+#' printer(r = rnorm(5), x = rnorm(5))
 
-printer <- function (r,x){
-  res <- tibble(r = r, x= x)
+printer <- function(r, x) {
+  res <- tibble(r = r, x = x)
   return(res)
 }
